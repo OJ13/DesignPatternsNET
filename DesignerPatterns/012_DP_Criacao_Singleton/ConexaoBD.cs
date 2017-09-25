b3ae373368764cd25f8c363e723261d56080db24
Desenvolvimento <Desenvolvimento@grupogtp.br>
2017-09-25 10:22:07 -0300

    Add Factory, Prototype and Singleton

---
+++ DesignerPatterns/012_DP_Criacao_Singleton/ConexaoBD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _012_DP_Criacao_Singleton
+{
+    public class ConexaoBD
+    {
+        // Singleton
+
+        //Criar ponto de entrada único
+        //guarda internamente a instância
+        private static ConexaoBD _instance;
+
+        //Esconde o construtor
+        protected ConexaoBD()
+        {
+
+        }
+        //Método que retorna o singleton
+        public static ConexaoBD Instance()
+        {
+            // Vai garantir que sempre uma instância exista, só uma
+            if(_instance == null)
+            {
+                _instance = new ConexaoBD();
+            }
+            return _instance;
+        }
+        public string stringConexao { get; set; }
+
+        public void Open()
+        {
+            Console.WriteLine("Abrindo conexao com o banco - " + stringConexao);
+        }
+    }
+}
---
+++ DesignerPatterns/012_DP_Criacao_Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _012_DP_Criacao_Singleton
+{
+    // Client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var con = ConexaoBD.Instance();
+            con.stringConexao = "Sql Server";
+            con.Open();
+            var con2 = ConexaoBD.Instance();
+            //Comprovando que são a mesma instância
+            if(con == con2) //esta referenciando a mesma posição de memória, apontando para o mesmo objeto
+            {
+                Console.WriteLine("Conexao são iguais");
+            }
+            con.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
